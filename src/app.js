7f08a7c6b397d40c1afcd89cbd8f27da07530af9
AndreSC1812 <andresalazar.dev@gmail.com>
2024-10-17 21:21:39 +0200

    chore: añadido configuración de CORS para permitir peticiones desde cualquier dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 //aqui estara todo el codigo del backend(express)
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 //importamos las rutas de autenticacion
 import authRoutes from "./routes/auth.routes.js";
 
 //usamos app como nuestra instancia de express
 const app = express();
+
+//usaremos cors para que cualquiera pueda hacer peticiones al backend
+app.use(cors());
 
 //usaremos morgan para que nos mande mensajes de las peticiones por consola
 app.use(morgan("dev"));
